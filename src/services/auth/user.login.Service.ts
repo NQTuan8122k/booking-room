1ce8cb5508f53c3fc08ea4b6e5ef11cd440f83be
nqt <nqtuan081200@gmail.com>
2023-05-23 18:49:52 +0700

    fix user login logic service

--- src/services/auth/user.login.Service.ts
+++ src/services/auth/user.login.Service.ts
@@ -10,69 +10,58 @@ export class UserLoginService {
   constructor(private readonly userRepository: UserRepository, private jwtTokenService: JwtTokenService) {}
 
   async login(loginData: UserLoginRequestDto) {
-    const userInfo = await this.userRepository.findOne({
-      username: loginData.username
-    });
-
-    const isMatchPassword = !!userInfo && (await bcrypt?.compare?.(loginData.password, userInfo?.password));
-
-    if (!!userInfo?.username && userInfo?.username == loginData.username && isMatchPassword && !!userInfo?.password) {
-      const token = await this.jwtTokenService.createAuthToken({
-        username: userInfo.username,
-        role: userInfo.role
+    try {
+      const userInfo = await this.userRepository.findOne({
+        username: loginData.username
       });
 
-      const responseData = new ResponseUserAuthDto(userInfo);
+      if (!userInfo?.username) {
+        throw new HttpException(
+          {
+            status: 400,
+            description: 'Username is not exist',
+            error_message: 'Username is not exist',
+            error_detail: null,
+            timestamp: new Date().toISOString()
+          },
+          HttpStatus.BAD_REQUEST
+        );
+      }
 
-      return { data: responseData, token };
-    }
+      const isMatchPassword = !!userInfo && (await bcrypt?.compare?.(loginData.password, userInfo?.password));
 
-    if (!userInfo?.username) {
+      if (!!userInfo?.username && userInfo?.username == loginData.username && isMatchPassword && !!userInfo?.password) {
+        const token = await this.jwtTokenService.createAuthToken({
+          username: userInfo.username,
+          role: userInfo.role
+        });
+
+        const responseData = new ResponseUserAuthDto(userInfo);
+
+        return { data: responseData, token };
+      } else {
+        throw new HttpException(
+          {
+            status: 400,
+            description: 'Wrong password',
+            error_message: 'Wrong password',
+            error_detail: null,
+            timestamp: new Date().toISOString()
+          },
+          HttpStatus.BAD_REQUEST
+        );
+      }
+    } catch (error) {
       throw new HttpException(
         {
-          status: 400,
-          description: 'Username is not exist',
-          error_message: 'Username is not exist',
+          status: 500,
+          description: 'Internal server login error not handle',
+          error_message: 'Internal server login error not handle',
           error_detail: null,
           timestamp: new Date().toISOString()
         },
         HttpStatus.BAD_REQUEST
       );
     }
-    if (userInfo?.username != loginData.username) {
-      throw new HttpException(
-        {
-          status: 400,
-          description: 'Wrong username',
-          error_message: 'Wrong username',
-          error_detail: null,
-          timestamp: new Date().toISOString()
-        },
-        HttpStatus.BAD_REQUEST
-      );
-    }
-    if (userInfo?.password != loginData.password) {
-      throw new HttpException(
-        {
-          status: 400,
-          description: 'Wrong password',
-          error_message: 'Wrong password',
-          error_detail: null,
-          timestamp: new Date().toISOString()
-        },
-        HttpStatus.BAD_REQUEST
-      );
-    }
-
-    throw new HttpException(
-      {
-        status: 500,
-        description: 'Internal server error not handle',
-        error_message: 'Internal server error not handle',
-        error_detail: null,
-        timestamp: new Date().toISOString()
-      },
-      HttpStatus.BAD_REQUEST
-    );
   }
 }
